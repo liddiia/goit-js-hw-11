a9b992a45c59aee0b245ada1926338778b667c90
liddiia <liddabojko@gmail.com>
2023-09-25 11:36:11 +0300

    Summury reset

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+
 const BASE_URL = "https://pixabay.com/api/";
 const KEY = "39646115-939d05d3332b2253cce455354";
+
 
 export async function fetchPhoto(q, page, perPage) {
     const url = `${BASE_URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
